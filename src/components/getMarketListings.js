dc5a4310ef9385fc6b58ebb66e592f541ada44e0
Olle Thunberg <olle.thunberg03@gmail.com>
2019-10-29 19:32:23 +0100

    design, logotyper och mer.

--- src/components/getMarketListings.js
+++ src/components/getMarketListings.js
@@ -5,7 +5,8 @@ export default class GetMarketListings extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            items: []
+            blocket: [],
+            tradera: [],
         }
     }
     componentDidMount(){
@@ -14,14 +15,19 @@ export default class GetMarketListings extends React.Component{
             for(let i = 0; i < response.blocket.length; i++){
                 if(response.blocket[i].title.length > 10) response.blocket[i].title = response.blocket[i].title.substr(0,10);
             }  
-            this.setState({items: response.blocket});
-            console.log(this.state.items);
+            this.setState({blocket: response.blocket});
+            for(let i = 0; i < response.blocket.length; i++){
+                if(response.tradera[i].title.length > 10) response.tradera[i].title = response.tradera[i].title.substr(0,10);
+            }  
+            this.setState({tradera: response.tradera});
         });
         
     }
     render(){
         return(<div>
-            {this.state.items.map(item => (<MarketItem  title={item.title} price={item.price} location="not set yet"></MarketItem>))}
+            <MarketItem title="En anna test sak mozilla firefox" price="450 000 kr" location="Osby" image="https://www.blocket.se/img/hub/logobyte_logo_310x200@2x.png" company="https://i.imgur.com/IcUn5Jr.png" link={"https://google.com"}></MarketItem>
+            {this.state.blocket.map(item => (<MarketItem  title={item.title} price={item.price} location="not set yet" image="https://i.imgur.com/IcUn5Jr.png" company="https://i.imgur.com/IcUn5Jr.png" link={item.link}></MarketItem>))}
+            {this.state.tradera.map(item => (<MarketItem  title={item.title} price={item.price} location="not set yet" image={item.image} company='https://i.imgur.com/y7yj6aD.png' link={item.link}></MarketItem>))}        
         </div>);
     }
 }
--- src/components/marketItem.js
+++ src/components/marketItem.js
@@ -10,7 +10,9 @@ export default class MarketItem extends React.Component{
             title: this.props.title,
             price: this.props.price,
             image: this.props.image,
-            location: this.props.location
+            location: this.props.location,
+            company: this.props.company,
+            link: this.props.link
         }
         
         this.classes = useStyles;
@@ -21,21 +23,21 @@ export default class MarketItem extends React.Component{
     render(){
 
         return(
-    <Card className={this.classes.card} style={{width: 400, height: 108, display: "inline-block", margin: 10}}>
+    <Card className={this.classes.card} style={{width: 400, height: 108, display: "inline-block", cursor: "pointer", margin: 10, position: "relative"}} onClick={() => {window.open(this.state.link, '_blank')}}>
         <CardContent style={{margin: 0, padding: 0}}>
-        <div className="imageDiv" style={{height: "100%", width: 192, padding: 0, margin: 0, display: "inline-block", float: "left"}}>
-            <img src="https://www.blocket.se/img/hub/logobyte_logo_310x200@2x.png" height="108" width="192" style={{margin: 0, padding: 0}}></img>
+        <div className="imageDiv" style={{height: "100%", width: 96, padding: 0, margin: 0, display: "inline-block", float: "left", textAlign: "center", alignContent:"center"}}>
+            <img src={this.state.image} height="54" width="96" style={{padding: 0, marginTop: 25, marginLeft: 20}}></img>
+        </div>
+        <div style={{position: "absolute", bottom: 5, right: 5}}>
+           <img src={this.state.company} height="25"></img>
         </div>
         <div style={{display: "inline-block"}}>
-            <h3 style={{marginTop: 5, marginBottom: 0}}>{this.state.title}</h3>
-            <p style={{margin: 0}}>{this.state.price}kr<br/>{this.state.location}</p>
+            <p style={{marginTop: 5, marginBottom: 0}}><em>{this.state.location}</em></p>
+            <h3 style={{margin: 0}}>{this.state.title}</h3>
+            <p style={{margin: 0}}>{this.state.price}</p>
+            
         </div>
         </CardContent>
-                <CardActions style={{margin: 0, padding: 0, textAlign: "center"}}>
-                <Button size="small" color="primary" style={{margin: "2% auto"}}>
-                Till annons
-                </Button>
-                </CardActions>
     </Card>
         )
     }

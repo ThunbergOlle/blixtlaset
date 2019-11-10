f232ecffc5cba8c1fa8a1a19439be37982a2b22f
Olle Thunberg <olle.thunberg03@gmail.com>
2019-11-10 13:53:04 +0100

    Footer fixad, håller nu sig permanent nere vid bottnen om man kollar från mobilen :+1:

--- src/App.js
+++ src/App.js
@@ -8,13 +8,20 @@ import './components/category.module.css';
 function App() {
 
   return (
-    <div className="App" id="app" style={{minHeight: "100%", marginBottom: "-50px"}}>
+    <div className="App" id="app">
     
       <NavBar></NavBar>
       <MainSearch></MainSearch>
       <div id="items">
         <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
-      
+        <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+        <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+        <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+        <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+        <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+        <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+        <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+
       </div>
 
       <Footer></Footer>
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FolderIcon from '@material-ui/icons/Folder';
-import RestoreIcon from '@material-ui/icons/Restore';
+import SearchIcon from '@material-ui/icons/Search';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-
+import HelpIcon from '@material-ui/icons/Help';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 const useStyles = makeStyles({
     root: {
       width: "100%",
+      position: 'fixed',
+      bottom: 0,
     },
   });
 export default function Footer() {
@@ -19,12 +20,22 @@ export default function Footer() {
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
+    const isMobile = window.innerWidth <= 500;
+    if(isMobile){
     return(
         <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
-        <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-        <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+        <BottomNavigationAction label="Sök" value="search" icon={<SearchIcon />} />
+        <BottomNavigationAction label="Hjälp" value="help" icon={<HelpIcon />} />
+        <BottomNavigationAction label="Sparade" value="saved" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Mitt konto" value="account" icon={<AccountCircleIcon />} />
         </BottomNavigation>
       )
+    }else {
+      return(
+        <div style={{display: 'none'}}>
+
+        </div>
+      );
+    }
+
 }

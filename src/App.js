65691c51a0ef771c4cffa5b81fd74cf95d2c7b3e
Olle Thunberg <olle.thunberg03@gmail.com>
2019-11-10 12:57:33 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/navbar';
 import MainSearch from './components/mainSearch';
+import Footer from './components/footer';
 import './components/category.module.css';
+
 function App() {
-  
+
   return (
-    <div className="App" id="app">
+    <div className="App" id="app" style={{minHeight: "100%", marginBottom: "-50px"}}>
+    
       <NavBar></NavBar>
       <MainSearch></MainSearch>
       <div id="items">
         <p>(Sök resultat kommer visas här, det ska vara filter just nu men har ej lagt till :])</p>
+      
       </div>
+
+      <Footer></Footer>
     </div>
   );
 }
+
 
 export default App;

3947b74c81e2af1a0dd67d2f9d454af0f3654255
Olle Thunberg <olle.thunberg03@gmail.com>
2019-11-23 16:39:47 +0100

    bug fixar

--- src/components/marketItem.js
+++ src/components/marketItem.js
@@ -16,12 +16,12 @@ export default class MarketItem extends React.Component{
         
     }
     render(){
-        
+        if(this.state.image === undefined) this.setState({image: "https://www.deadlineclaims.com/wp-content/uploads/2017/02/placeholder-image.jpg"})
         return(
-    <Card className={this.classes.card} style={{width: 600, height: 108, display: "inline-block", cursor: "pointer", margin: 10, position: "relative"}} onClick={() => {window.open(this.state.link, '_blank')}}>
+    <Card style={{width: 600, height: 108, display: "inline-block", cursor: "pointer", margin: 10, position: "relative"}} onClick={() => {window.open(this.state.link, '_blank')}}>
         <CardContent style={{margin: 0, padding: 0}}>
         <div className="imageDiv" style={{height: "100%", width: 96, padding: 0, margin: 0, display: "inline-block", float: "left", textAlign: "center", alignContent:"center"}}>
-            <img src={this.state.image} alt="item" height="108" width="192" style={{padding: 0}}></img>
+            <img src={this.state.image} alt="item" height="108" style={{padding: 0, textAlign: 'center', margin: 'auto'}}></img>
         </div>
         <div style={{position: "absolute", bottom: 5, right: 5}}>
            <img src={this.state.company} alt="company" height="25"></img>
